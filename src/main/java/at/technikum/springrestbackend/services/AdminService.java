542ec4240e701bef4e6f768ad9fb49a4fa46fda2
christopherhois <hois.cc@gmail.com>
2024-10-17 11:03:02 +0200

    minor spelling change

--- src/main/java/at/technikum/springrestbackend/services/AdminService.java
+++ src/main/java/at/technikum/springrestbackend/services/AdminService.java
@@ -1,26 +1,26 @@
-package at.technikum.springrestbackend.service;
+package at.technikum.springrestbackend.services;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
 
-    public String deleteUser(Long userId) {
+    public String deleteUser(String userId) {
         // Hier sollte die Logik stehen, um den Benutzer in der Datenbank zu deaktivieren
         return "User mit ID " + userId + " wurde gelöscht.";
     }
 
-    public String restoreUser(Long userId) {
+    public String restoreUser(String userId) {
         // Hier sollte die Logik stehen, um den Benutzer in der Datenbank wiederherzustellen
         return "User mit ID " + userId + " wurde wiederhergestellt.";
     }
 
-    public String deleteEvent(Long eventId) {
+    public String deleteEvent(String eventId) {
         // Hier sollte die Logik stehen, um das Event in der Datenbank zu deaktivieren
         return "Event mit ID " + eventId + " wurde gelöscht.";
     }
 
-    public String restoreEvent(Long eventId) {
+    public String restoreEvent(String eventId) {
         // Hier sollte die Logik stehen, um das Event in der Datenbank wiederherzustellen
         return "Event mit ID " + eventId + " wurde wiederhergestellt.";
     }
